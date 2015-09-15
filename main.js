48da80a790a5d55c12da00b9ed1be5c165fa7d77
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-09-15 11:57:19 -0700

    Update 2015-09-15T18:57:11.845Z

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 PIXI.loader
     .add('res/images.json')
     .add('res/fonts/opendyslexic.fnt')
+    .add('res/fonts/carrier_command.fnt')
     .load(function(loader, resources) {
         new example.core.SpaceshipWarrior(resources);
     });
